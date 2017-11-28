c0b6ef13989517a9e57475441c5820ee664b3324
BeckettMorsch <beckettmorsch@gmail.com>
2017-11-28 12:31:29 -0500

    started the markov

--- MarkovTextGenerator/Chain.cs
+++ MarkovTextGenerator/Chain.cs
@@ -37,7 +37,17 @@ namespace MarkovTextGenerator
         public void AddString (String sentence)
         {
             // TODO: Break sentence up into word pairs
+            sentence = new string(sentence.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            string[] words = sentence.Split(' ');
+
             // TODO: Add each word pair to the chain
+            for (int i = 0; i < words.Length; i++)
+            {
+                AddPair(words[i], words[i + 1]);
+            }
+            AddPair(words[words.Length], "");
+
         }
 
         // Adds a pair of words to the chain that will appear in order
@@ -83,6 +93,8 @@ namespace MarkovTextGenerator
                 foreach (Word s in words[word])
                 {
                     // TODO: Finish me
+
+
                 }
 
                 // Step 2:  Update the probabilities
