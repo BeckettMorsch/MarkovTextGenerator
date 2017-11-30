8b197b351847575577ad8b801d8e6c218fcd3eda
BeckettMorsch <beckettmorsch@gmail.com>
2017-11-30 12:33:45 -0500

    finshed first part

--- MarkovTextGenerator/Chain.cs
+++ MarkovTextGenerator/Chain.cs
@@ -9,11 +9,13 @@ namespace MarkovTextGenerator
     class Chain
     {
         public Dictionary<String, List<Word>> words;
+        private Dictionary<String, int> sums;
         private Random rand;
 
         public Chain ()
         {
             words = new Dictionary<String, List<Word>>();
+            sums = new Dictionary<string, int>();
             rand = new Random(System.Environment.TickCount);
         }
 
@@ -37,16 +39,16 @@ namespace MarkovTextGenerator
         public void AddString (String sentence)
         {
             // TODO: Break sentence up into word pairs
-            sentence = new string(sentence.Where(c => !char.IsPunctuation(c)).ToArray());
+            sentence = new String(sentence.Where(c => !char.IsPunctuation(c)).ToArray());
 
-            string[] words = sentence.Split(' ');
+            String[] words = sentence.Split(' ');
 
             // TODO: Add each word pair to the chain
-            for (int i = 0; i < words.Length; i++)
+            for (int i = 0; i < words.Length-1; i++)
             {
                 AddPair(words[i], words[i + 1]);
             }
-            AddPair(words[words.Length], "");
+            AddPair(words[words.Length-1], "");
 
         }
 
@@ -55,17 +57,27 @@ namespace MarkovTextGenerator
         {
             if (!words.ContainsKey(word))
             {
+                sums.Add(word, 1);
                 words.Add(word, new List<Word>());
                 words[word].Add(new Word(word2));
             }
             else
             {
+                bool found = false;
                 foreach (Word s in words[word])
                 {
                     if (s.ToString() == word2)
                     {
+                        found = true;
                         s.Count++;
+                        sums[word]++;
                     }
+                }
+
+                if (!found)
+                {
+                    words[word].Add(new Word(word2));
+                    sums[word]++;
                 }
             }
         }
@@ -75,9 +87,21 @@ namespace MarkovTextGenerator
         {
             if (words.ContainsKey(word))
             {
-                double choice = 1.0 / (double)rand.Next(100000);
+                double choice = rand.NextDouble();
 
-                Console.WriteLine("I picked the number " + choice); 
+                Console.WriteLine("I picked the number " + choice);
+
+                List<Word> poss = words[word];
+
+                double sum = 0;
+
+                foreach( Word p in poss)
+                {
+                    sum += p.Probability;
+
+                    if (sum > choice)
+                        return p.ToString();
+                }
             }
 
             return "idkbbq";
@@ -89,18 +113,14 @@ namespace MarkovTextGenerator
             {
                 double sum = 0;  // Total sum of all the occurences of each followup word
 
-                // Step 1:  Get the sum of all occurences of each followup word
+                
+
+                // Update the probabilities
                 foreach (Word s in words[word])
                 {
-                    // TODO: Finish me
-
-
-                }
-
-                // Step 2:  Update the probabilities
-                foreach (Word s in words[word])
-                {
-                    // TODO: Finish me
+                    
+                    s.Probability = (double)s.Count /
+                        sums[word];
                 }
 
             }
